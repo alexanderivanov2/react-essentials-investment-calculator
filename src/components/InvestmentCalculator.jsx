5700fb3fcffc194fea079c28264dda687ab40cfe
alexanderivanov2 <alexanderivanov2277@gmail.com>
2025-03-29 21:33:14 +0200

    get calculateInvestmentResults

--- src/components/InvestmentCalculator.jsx
+++ src/components/InvestmentCalculator.jsx
@@ -1,47 +1,35 @@
 import React, { useState } from 'react'
+import InvestmentForm from './InvestmentForm';
+import { calculateInvestmentResults } from '../util/investment';
 
 function InvestmentCalculator() {
     const [investmentData, setInvestmentData] = useState({
-        initial_investment: 0,
-        annual_investment: 0,
-        expected_return: 0,
+        initialInvestment: 0,
+        annualInvestment: 0,
+        expectedReturn: 0,
         duration: 0,
     });
 
-    const handleInputChange = (e, inputName) => {
-        const value = Number(e.target.value)
+    const investmentResults = calculateInvestmentResults(investmentData)
+    console.dir(investmentResults)
+    const handleInvestmentDataChange = (inputName, value) => {
         setInvestmentData(prevInvestmentData => ({
             ...prevInvestmentData,
-            [inputName]: value >= 0 ? value : 0
-        }))
+            [inputName]: value
+        }));
     }
 
     return (
-        <div>InvestmentCalculator
-            <div id="user-input">
-                <div className="input-group">
-                    <div>
-                        <label htmlFor="">Initial Investment</label>
-                        <input type="number" value={investmentData.initial_investment} onChange={(e) => handleInputChange(e, 'initial_investment')}/>
-                    </div>
-                    <div>
-                        <label htmlFor="">Anual Investment</label>
-                        <input type="number" value={investmentData.annual_investment} onChange={(e) => handleInputChange(e, 'annual_investment')}/>
-                    </div>
-                    <div>
-                        <label htmlFor="">Expected Return</label>
-                        <input type="number" value={investmentData.expected_return} onChange={(e) => handleInputChange(e, 'expected_return')}/>
-                    </div>
-                    <div>
-                        <label htmlFor="">Duration</label>
-                        <input type="number" value={investmentData.duration} onChange={(e) => handleInputChange(e, 'duration')}/>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <InvestmentForm investmentData={investmentData} handleInvestmentDataChange={handleInvestmentDataChange}/>
+            
+            
 
-            <p>Initial Investment: { investmentData.initial_investment }</p>
-            <p>Anual Investment: { investmentData.annual_investment }</p>
-            <p>EXPECTED RETURN: { investmentData.expected_return }</p>
+
+
+            <p>Initial Investment: { investmentData.initialInvestment }</p>
+            <p>Anual Investment: { investmentData.annualInvestment }</p>
+            <p>EXPECTED RETURN: { investmentData.expectedReturn }</p>
             <p>DURATION: { investmentData.duration}</p>
         </div>
     )
